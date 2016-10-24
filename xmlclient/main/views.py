9bc0e4d9c09628df43c0077fee09323119212cdb
David Griffiths <dgriff_67@yahoo.com>
2016-10-24 18:00:45 +0100

    added views for setting hometeam filter and listing matches subject to filter

--- xmlclient/main/views.py
+++ xmlclient/main/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from .models import Pl1516
+
+def index(request):
+    if not request.GET.get('hometeam'):
+        return render(request, 'main/index.html')
+    else:
+        hometeam_filter = request.GET.get('hometeam')
+        matches = Pl1516.objects.filter(hometeam=hometeam_filter)
+        context_dict = {'matches' : matches}
+        return render(request, 'main/match_listings.html', context_dict)
+
