a9df4ddc21ff015aa1b0ab75d77357359ce6cdae
David Griffiths <dgriff_67@yahoo.com>
2016-10-26 17:12:57 +0100

    setting filters from the HTTP request and adding these to the context dictionary

--- xmlclient/main/views.py
+++ xmlclient/main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.template import RequestContext
 
 # Create your views here.
 from django.http import HttpResponse
@@ -11,10 +12,12 @@ def index(request):
         return render(request, 'main/index.html')
     else:
         hometeam_filter = request.GET.get('hometeam')
+        hthg_filter = request.GET.get('HTHG')
+        htag_filter = request.GET.get('HTAG')
         matches = Pl1516.objects.filter(hometeam=hometeam_filter)
         if (matches.count() < 19):
             parse_load(hometeam_filter)
-        context_dict = {'matches' : matches}
+        context_dict = {'hthg_filter': hthg_filter, 'htag_filter': htag_filter, 'matches' : matches}
         return render(request, 'main/match_listings.html', context_dict)
 
 
