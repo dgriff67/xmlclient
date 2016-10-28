db213b78417c4a2060a4333709bc0cd0062e3c88
David Griffiths <dgriff_67@yahoo.com>
2016-10-28 16:25:19 +0100

    now build context_dict by iterating through request.GET.items. Program flow depending upon filtering by hometeam or awayteam or upon neither

--- xmlclient/main/views.py
+++ xmlclient/main/views.py
@@ -4,38 +4,37 @@ from django.template import RequestContext
 # Create your views here.
 from django.http import HttpResponse
 from .models import Pl1516
-from .parse import parse_load
+from .parse import parse_load, parse_load_all
 
+filters = ['ht', 'htr', 'ftr', 'ref', 's', 'st', 'c', 'fc', 'yc', 'rc', 'b365', 'lad', 'vc', 'wh', 'bb25']
 
 def index(request):
+    context_dict = {(k + '_filter'): v for k, v in request.GET.items() if k in filters}
+    hometeam_filter = request.GET.get('hometeam')
+    awayteam_filter = request.GET.get('awayteam')
+
     if not request.GET.get('hometeam'):
         return render(request, 'main/index.html')
-    else:
-        hometeam_filter = request.GET.get('hometeam')
-        ht_filter = request.GET.get('HT')
-        htr_filter = request.GET.get('HTR')
-        ftr_filter = request.GET.get('FTR')
-        ref_filter = request.GET.get('Ref')
-        s_filter = request.GET.get('S')
-        st_filter = request.GET.get('ST')
-        c_filter = request.GET.get('C')
-        fc_filter = request.GET.get('FC')
-        yc_filter = request.GET.get('YC')
-        rc_filter = request.GET.get('RC')
-        b365_filter = request.GET.get('B365')
-        lad_filter = request.GET.get('Lad')
-        vc_filter = request.GET.get('VC')
-        wh_filter = request.GET.get('WH')
-        bb25_filter = request.GET.get('BB2.5')
 
+    elif request.GET.get('awayteam')=='Any' and request.GET.get('hometeam')!='Any':
         matches = Pl1516.objects.filter(hometeam=hometeam_filter)
         if matches.count() < 19:
-            parse_load(hometeam_filter)
-        context_dict = {'ht_filter': ht_filter, 'htr_filter': htr_filter,  'ftr_filter': ftr_filter,
-                        'ref_filter': ref_filter,'s_filter': s_filter, 'st_filter': st_filter,
-                        'c_filter': c_filter,'fc_filter': fc_filter,'yc_filter': yc_filter,'rc_filter': rc_filter,
-                        'b365_filter': b365_filter, 'lad_filter': lad_filter, 'vc_filter': vc_filter, 'wh_filter': wh_filter,
-                        'bb25_filter': bb25_filter, 'matches' : matches}
-        return render(request, 'main/match_listings.html', context_dict)
+            parse_load('home', hometeam_filter)
 
+    elif request.GET.get('hometeam') == 'Any' and request.GET.get('awayteam') != 'Any':
+        matches = Pl1516.objects.filter(awayteam=awayteam_filter)
+        if matches.count() < 19:
+            parse_load('away', awayteam_filter)
 
+    elif request.GET.get('hometeam') != 'Any' and request.GET.get('awayteam') != 'Any':
+        matches = Pl1516.objects.filter(hometeam=hometeam_filter, awayteam=awayteam_filter)
+        if matches.count() < 1:
+            parse_load('away', awayteam_filter)
+
+    elif request.GET.get('hometeam') == 'Any' and request.GET.get('awayteam') == 'Any':
+        matches = Pl1516.objects.all()
+        if matches.count() < 380:
+            parse_load_all
+
+    context_dict['matches'] = matches
+    return render(request, 'main/match_listings.html', context_dict)
