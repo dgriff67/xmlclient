a3418db9b1e756dc6fdf8e1c497ef7da92d7f199
David Griffiths <dgriff_67@yahoo.com>
2016-10-28 16:19:11 +0100

    adjusted parse_load to parse and load home or away teams. Also added parse_load_all to parse and load all matches

--- xmlclient/main/parse.py
+++ xmlclient/main/parse.py
@@ -4,11 +4,11 @@ import requests
 from .models import Pl1516
 
 
-baseUrl = 'http://dgriff67.pythonanywhere.com/api/Pl1516/?format=xml&hometeam='
+baseUrl = 'http://dgriff67.pythonanywhere.com/api/Pl1516/?format=xml'
 data_dict = dict()
 
-def parse_load(hometeam):
-    url = baseUrl+hometeam
+def parse_load(venue, team):
+    url = baseUrl+'&'+venue+'team='+team
     resp = requests.get(url)
     msg = resp.content
     tree = ET.fromstring(msg)
@@ -18,4 +18,20 @@ def parse_load(hometeam):
                 data_dict.update({child.tag : child.text})
             m = Pl1516(**data_dict)
             m.save()
+
+def parse_load_all():
+    teams = ['Arsenal','Aston Villa', 'Bournemouth', 'Chelsea', 'Crystal Palace', 'Everton', 'Leicester',
+             'Liverpool', 'Man City', 'Man United', 'Newcastle', 'Norwich', 'Southampton', 'Stoke', 'Sunderland',
+             'Swansea', 'Tottenham', 'Watford', 'West Brom', 'West Ham']
+    for team in teams:
+        url = baseUrl+'&hometeam='+team
+        resp = requests.get(url)
+        msg = resp.content
+        tree = ET.fromstring(msg)
+        for objects in tree.findall('objects'):
+            for object in objects:
+                for child in object:
+                    data_dict.update({child.tag : child.text})
+                m = Pl1516(**data_dict)
+                m.save()
 
